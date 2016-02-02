2adaf07720e8f47135c7f95bd885faa2a8725118
Boarsteas <boarsteas@me.com>
2016-02-02 19:45:07 +0100

    läuft bei uns

--- Client/MyClient.java
+++ Client/MyClient.java
@@ -1,38 +1,22 @@
-package Client;
-
-/**
- * Created by BSK on 31/01/16.
- */
-
-import java.io.IOException;
 import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.MulticastSocket;
-import java.net.UnknownHostException;
+//from  w w  w  . j a  v  a2 s  .c  o  m
 public class MyClient {
-    final static String INET_ADDR = "224.0.0.3";
-    final static int PORT = 8888;
-    public static void main(String[] args) throws UnknownHostException {
-        // Get the address that we are going to connect to.
-        InetAddress address = InetAddress.getByName(INET_ADDR);
-        // Create a buffer of bytes, which will be used to store
-        // the incoming bytes containing the information from the server.
-        // Since the message is small here, 256 bytes should be enough.
-        byte[] buf = new byte[256];
-        // Create a new Multicast socket (that will allow other sockets/programs
-        // to join it as well.
-        try (MulticastSocket clientSocket = new MulticastSocket(PORT)) {
-            //Joint the Multicast group.
-            clientSocket.joinGroup(address);
-            while (true) {
-                // Receive the information and print it.
-                DatagramPacket msgPacket = new DatagramPacket(buf, buf.length);
-                clientSocket.receive(msgPacket);
-                String msg = new String(buf, 0, buf.length);
-                System.out.println("Socket 1 received msg: " + msg);
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
+    public static void main(String[] args) throws Exception {
+        int mcPort = 12345;
+        String mcIPStr = "230.1.1.1";
+        DatagramSocket udpSocket = new DatagramSocket();
+
+        InetAddress mcIPAddress = InetAddress.getByName(mcIPStr);
+        byte[] msg = "Hello".getBytes();
+        DatagramPacket packet = new DatagramPacket(msg, msg.length);
+        packet.setAddress(mcIPAddress);
+        packet.setPort(mcPort);
+        udpSocket.send(packet);
+
+        System.out.println("Sent a  multicast message.");
+        System.out.println("Exiting application");
+        udpSocket.close();
     }
 }
--- Server/MyServer.java
+++ Server/MyServer.java
@@ -1,36 +1,30 @@
-
 package Server;
-import java.io.IOException;
+
 import java.net.DatagramPacket;
-import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
-/**
- * Created by Florian on 31.01.2016.
- */
-
-
+import java.net.MulticastSocket;
+/*ww  w. j  av  a  2 s .  com*/
 public class MyServer {
-    final static String INET_ADDR = "224.0.0.3";
-    final static int PORT = 8888;
-    public static void main(String[] args) throws UnknownHostException, InterruptedException {
-        // Get the address that we are going to connect to.
-        InetAddress addr = InetAddress.getByName(INET_ADDR);
-        // Open a new DatagramSocket, which will be used to send the data.
-        try (DatagramSocket serverSocket = new DatagramSocket()) {
-            for (int i = 0; i < 5; i++) {
-                String msg = "Sent message no " + i;
-                // Create a packet that will contain the data
-                // (in the form of bytes) and send it.
-                DatagramPacket msgPacket = new DatagramPacket(msg.getBytes(),
-                        msg.getBytes().length, addr, PORT);
-                serverSocket.send(msgPacket);
-                System.out.println("Server sent packet with msg: " + msg);
-                Thread.sleep(500);
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
+    public static void main(String[] args) throws Exception {
+        int mcPort = 12345;
+        String mcIPStr = "230.1.1.1";
+        MulticastSocket mcSocket = null;
+        InetAddress mcIPAddress = null;
+        mcIPAddress = InetAddress.getByName(mcIPStr);
+        mcSocket = new MulticastSocket(mcPort);
+        System.out.println("Multicast Receiver running at:"
+                + mcSocket.getLocalSocketAddress());
+        mcSocket.joinGroup(mcIPAddress);
 
-        }}}
-socket.close();
+        DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
 
+        System.out.println("Waiting for a  multicast message...");
+        mcSocket.receive(packet);
+        String msg = new String(packet.getData(), packet.getOffset(),
+                packet.getLength());
+        System.out.println("[Multicast  Receiver] Received:" + msg);
+
+        mcSocket.leaveGroup(mcIPAddress);
+        mcSocket.close();
+    }
+}
